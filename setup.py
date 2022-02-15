f12e54d328f5bcbe5ebafc70c20b7143e3647a58
xushengfeng <xushengfeng_zg@163.com>
2022-02-15 23:23:13 +0800

    :sparkles:安装顺序调换

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
 import platform
 import os
 
+
+print('安装相关库')
+os.system('pip install -r requirements.txt')
+os.system('pip install paddlepaddle==2.2.2 -i https://mirror.baidu.com/pypi/simple')
+
+print('初始化PaddleOCR')
+
+from paddleocr import PaddleOCR
+ocr = PaddleOCR(use_gpu=False)  # 首次执行会自动下载模型文件
+
+import requests
+import shutil
+kenlmpath = os.path.exists("kenlm-master")
+if not kenlmpath:
+    print('下载kenlm库中')
+    file_url ="https://github.com/kpu/kenlm/archive/master.zip"
+    res = requests.get(url=file_url)
+    with open(r'kenlm-master.zip',mode='wb') as f:  # 需要用wb模式
+        f.write(res.content)
+    shutil.unpack_archive(filename=r'kenlm-master.zip',extract_dir=r'./',format='zip')
+
+
+print("进入目录")
+os.chdir("kenlm-master")
+print('安装kenlm中')
+os.system('python setup.py install')
+
 pla = platform.system()
 if pla == "Windows":
     path = os.path.expandvars(r"%APPDATA%\eSearch\service-installed")
@@ -15,29 +42,3 @@ print(path)
 folder = os.path.exists(path)
 if not folder:
     os.makedirs(path)
-
-import requests
-import shutil
-kenlmpath = os.path.exists("kenlm-master")
-if not kenlmpath:
-    print('下载kenlm库中')
-    file_url ="https://github.com/kpu/kenlm/archive/master.zip"
-    res = requests.get(url=file_url)
-    with open(r'kenlm-master.zip',mode='wb') as f:  # 需要用wb模式
-        f.write(res.content)
-    shutil.unpack_archive(filename=r'kenlm-master.zip',extract_dir=r'./',format='zip')
-
-import os
-
-print('安装相关库')
-os.system('pip install -r requirements.txt')
-
-print("进入目录")
-os.chdir("kenlm-master")
-print('安装kenlm中')
-os.system('python setup.py install')
-
-print('初始化PaddleOCR')
-
-from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_gpu=False)  # 首次执行会自动下载模型文件
