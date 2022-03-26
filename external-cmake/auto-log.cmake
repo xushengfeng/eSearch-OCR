36cb25c2323228782cd92d3b97f573c8ae45fdde
xushengfeng <xushengfeng_zg@163.com>
2022-03-26 18:44:53 +0800

    :hammer:make文件更新

--- external-cmake/auto-log.cmake
+++ external-cmake/auto-log.cmake
@@ -6,7 +6,7 @@ set(FETCHCONTENT_BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}/third-party")
 FetchContent_Declare(
   extern_Autolog
   PREFIX autolog
-  GIT_REPOSITORY https://github.com/LDOUBLEV/AutoLog.git
+  GIT_REPOSITORY https://hub.fastgit.xyz/LDOUBLEV/AutoLog.git
   GIT_TAG        main
 )
 FetchContent_MakeAvailable(extern_Autolog)
