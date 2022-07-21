9cef269074dc42570885a172a04858c5aa2362d2
xushengfeng <xushengfeng_zg@163.com>
2022-07-21 21:54:47 +0800

    windows安装脚本

--- py/install2.bat
+++ py/install2.bat
@@ -1,4 +1,4 @@
 python3.7 -m venv env
-source env/bin/activate
+env\Scripts\activate.bat
 pip install -r ppocr/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install paddlepaddle -i https://pypi.tuna.tsinghua.edu.cn/simple
