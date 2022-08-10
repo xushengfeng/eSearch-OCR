288eda5b2f92a2bdd895d177f90e8abaf70d31ad
xushengfeng <xushengfeng_zg@163.com>
2022-08-10 21:08:38 +0800

    删除不用的代码

--- py/install2.bat
+++
@@ -1,4 +0,0 @@
-python3.7 -m venv env
-env\Scripts\activate.bat
-pip install -r ppocr/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install paddlepaddle -i https://pypi.tuna.tsinghua.edu.cn/simple
