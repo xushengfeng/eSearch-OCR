94854360cc9f9119eb3f90f47a2979dcad0fb9f4
xushengfeng <xushengfeng_zg@163.com>
2022-10-27 17:50:46 +0800

    主要 允许切换web和node后端

--- js/test.js
+++ js/test.js
@@ -8,6 +8,7 @@ async function start() {
         rec_path: "./m/ch_PP-OCRv3_rec_infer.onnx",
         dic_path: "../assets/ppocr_keys_v1.txt",
         dev: true,
+        node: true,
     });
     let img = document.createElement("img");
     img.src = "../a.png";
