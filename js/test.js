2ecf80a929b061de83c3a9099e10d26a5ad0aa5b
xushengfeng <xushengfeng_zg@163.com>
2022-10-30 15:52:19 +0800

    测试 输出文字

--- js/test.js
+++ js/test.js
@@ -17,6 +17,14 @@ async function start() {
         canvas.width = img.width;
         canvas.height = img.height;
         canvas.getContext("2d").drawImage(img, 0, 0);
-        x.ocr(canvas.getContext("2d").getImageData(0, 0, img.width, img.height));
+        x.ocr(canvas.getContext("2d").getImageData(0, 0, img.width, img.height)).then((v) => {
+            let tl = [];
+            for (let i of v) {
+                tl.push(i.text);
+            }
+            let p = document.createElement("p");
+            p.innerText = tl.join("\n");
+            document.body.append(p);
+        });
     };
 }
