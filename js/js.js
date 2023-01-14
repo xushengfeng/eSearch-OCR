38b6e6f613d864f5d254a1e03699d91baa73e87a
xushengfeng <xushengfeng_zg@163.com>
2023-01-14 18:02:03 +0800

    主要 匹配所有字典换行符

--- js/js.js
+++ js/js.js
@@ -27,7 +27,7 @@ async function init(x) {
     dev = x.dev;
     det = await ort.InferenceSession.create(x.det_path);
     rec = await ort.InferenceSession.create(x.rec_path);
-    dic = x.dic.split("\n");
+    dic = x.dic.split(/\r\n|\r|\n/);
     if (x.max_side) limit_side_len = x.max_side;
     if (x.imgh) imgH = x.imgh;
     if (x.imgw) imgW = x.imgw;
