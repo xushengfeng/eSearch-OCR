1d86a071bbd3033915fcb2f690a92c19dda8ee8c
xushengfeng <xushengfeng_zg@163.com>
2026-08-13 16:43:48 +0800

    开发 导出dts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
     // 打包配置
@@ -9,4 +10,5 @@ export default defineConfig({
             name: "esearch-ocr",
         },
     },
+    plugins: [dts({ rollupTypes: true })],
 });
